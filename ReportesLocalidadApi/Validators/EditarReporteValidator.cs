1e5700dcec66b51f5e8addc03821fc5afc0b108b
Ramiro0318 <ramiro.val03@gmail.com>
2026-05-19 21:45:34 -0600

    Validators.

---
+++ ReportesLocalidadApi/Validators/EditarReporteValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using ReportesLocalidadApi.Models.DTOs;
+
+namespace ReportesLocalidadApi.Validators
+{
+    public class EditarReporteValidator : AbstractValidator<SubirReporteDto>
+    {
+        public EditarReporteValidator()
+        {
+            RuleFor(x => x.Titulo).NotEmpty().WithMessage("Ingrese un título para el reporte.")
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("El título no puede contener solo espacios vacíos.")
+                .MinimumLength(3).WithMessage("Ingrese un título con al menos 3 caracteres.")
+                .MaximumLength(120).WithMessage("Ingrese un título con un máximo de 120 caracteres.")
+                .Matches(@"^(?=.*[a-zA-ZñÑ]).+$").WithMessage("El título debe contener al menos una letra.");
+
+            RuleFor(x => x.Descripcion).NotEmpty().WithMessage("Ingrese una descripción para el reporte.")
+               .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("La descripción no puede contener solo espacios vacíos.")
+               .MaximumLength(800).WithMessage("Ingrese una descripción con un máximo de 800 caracteres.");
+
+
+            RuleFor(x => x.Direccion).MaximumLength(500).WithMessage("Ingrese una dirección con un máximo de 500 caracteres.");
+
+            RuleFor(x => x.IdCategoria).GreaterThan(0).WithMessage("Categoría no válida.")
+                .LessThan(7).WithMessage("Categoría no válida.");
+        }
+    }
+}
--- ReportesLocalidadApi/Validators/RegistroValidator.cs
+++ ReportesLocalidadApi/Validators/RegistroValidator.cs
@@ -3,19 +3,21 @@ using ReportesLocalidadApi.Models.DTOs;
 
 namespace ReportesLocalidadApi.Validators
 {
-    public class LoginValidator: AbstractValidator<LoginDto>
+    public class RegistroValidator : AbstractValidator<RegistroDto>
     {
-        public LoginValidator()
+        public RegistroValidator()
         {
             RuleFor(x => x.NombreUsuario).NotEmpty().WithMessage("El nombre de usuario es obligatorio.")
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("El título no puede contener solo espacios vacíos.")
                 .MinimumLength(1).WithMessage("Ingrese un nombre de usuario válido.")
                 .MaximumLength(40).WithMessage("Un nombre de usuario no puede tener tantas letras.")
-                .Matches(@"^[a-zA-Z0-9 ]+$").WithMessage("Un nombre no puede contener simbolos ni caracteres especiales.");
+                .Matches(@"^[a-zA-Z0-9ñÑ]+$").WithMessage("Un nombre no puede contener simbolos ni caracteres especiales.");
 
             RuleFor(x => x.Password).NotEmpty().WithMessage("La contraseña no puede estar vacía.").
                 MinimumLength(6).WithMessage("La contraseña tiene que tener entre 6 y 30 caracteres.").
                 MaximumLength(30).WithMessage("La contraseña tiene que tener entre 6 y 30 caracteres.");
+
+            RuleFor(x => x.IdRol).Equal(1).WithMessage("Rol no válido").Equal(2).WithMessage("Rol no válido");
         }
     }
-
 }
